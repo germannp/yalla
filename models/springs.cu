e5a008135f55e6d50ded43f8e5309a99ec46e707
Philipp Germann <philipp.germann@crg.eu>
2015-10-30 14:56:42 +0100

    Move sorting.cu to springs.cu

--- models/springs.cu
+++ models/springs.cu
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <cmath>
 
 #include "../lib/vtk.cu"
@@ -8,9 +9,9 @@
 
 __device__ __managed__ float3 X[N_CELLS];
 
-int main(int argc, char const *argv[]) {
+int main(int argc, const char* argv[]) {
     float r_min = 0.5;
-    float r_max = pow(N_CELLS/0.75, 1./3)*r_min/2; /* Sphere packing */
+    float r_max = pow(N_CELLS/0.75, 1./3)*r_min/2; // Sphere packing
     for (int i = 0; i < N_CELLS; i++) {
         float r = r_max*rand()/(RAND_MAX + 1.);
         float theta = rand()/(RAND_MAX + 1.)*2*M_PI;
@@ -19,6 +20,12 @@ int main(int argc, char const *argv[]) {
         X[i].y = r*cos(theta)*sin(phi);
         X[i].z = r*cos(phi);
     }
-    write_positions("descht.vtk", N_CELLS, X);
+
+    for (int time_step = 0; time_step < 10; time_step++) {
+        std::stringstream file_name;
+        file_name << "springs_" << time_step << ".vtk";
+        write_positions(file_name.str().c_str(), N_CELLS, X);
+    }
+
     return 0;
 }
