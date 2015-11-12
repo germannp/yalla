fc08af40e3ef392a79b7db20251838f9aa03e41c
Philipp Germann <philipp.germann@crg.eu>
2015-11-12 10:19:40 +0100

    Add util.cuh for float atomicAdd()

---
+++ lib/util.cuh
@@ -0,0 +1,12 @@
+// atomicMax for floats, after http://stackoverflow.com/questions/17399119/
+__device__ static float atomicMax(float* address, float val)
+{
+    int* address_as_i = (int*) address;
+    int old = *address_as_i, assumed;
+    do {
+        assumed = old;
+        old = ::atomicCAS(address_as_i, assumed,
+            __float_as_int(::fmaxf(val, __int_as_float(assumed))));
+    } while (assumed != old);
+    return __int_as_float(old);
+}
