f667917aace6f039fa0282191945ae8d5a7ffd72
Philipp Germann <philipp.germann@crg.eu>
2015-11-25 15:52:07 +0100

    Add noise

--- models/sorting_lj.cu
+++ models/sorting_lj.cu
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <cmath>
 #include <sys/stat.h>
+#include <curand_kernel.h>
 
 #include "../lib/sphere.cuh"
 #include "../lib/vtk.cuh"
@@ -16,6 +17,7 @@ const int SKIP_STEPS = 100;
 const float DELTA_T = 0.0001;
 
 __device__ __managed__ float3 X[N_CELLS];
+__device__ __managed__ curandState rand_states[N_CELLS];
 
 
 __device__ float3 cell_cell_interaction(float3 Xi, float3 Xj, int i, int j) {
@@ -28,12 +30,19 @@ __device__ float3 cell_cell_interaction(float3 Xi, float3 Xj, int i, int j) {
         float r_rel = R_MIN/dist;
         float F = powf(r_rel, 13);
         F -= powf(r_rel, 7);
+        F += curand_normal(&rand_states[i])*10/sqrtf(N_CELLS);
         dF.x = strength*r.x*F/dist;
         dF.y = strength*r.y*F/dist;
         dF.z = strength*r.z*F/dist;
     }
     assert(dF.x == dF.x); // For NaN f != f.
     return dF;
+}
+
+
+__global__ void setup_rand_states() {
+    int i = blockIdx.x*blockDim.x + threadIdx.x;
+    if (i < N_CELLS) curand_init(1337, i, 0, &rand_states[i]);
 }
 
 
@@ -44,6 +53,8 @@ int main(int argc, char const *argv[]) {
     for (int i = 0; i < N_CELLS; i++) {
         cell_type[i] = (i < N_CELLS/2) ? 0 : 1;
     }
+    setup_rand_states<<<(N_CELLS + 32 - 1)/32, 32>>>();
+    cudaDeviceSynchronize();
 
     // Integrate cell positions
     mkdir("output", 755);
