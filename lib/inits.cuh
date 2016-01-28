ba61c02dec3bed785053db23f2187be0275ee5d0
Philipp Germann <philipp.germann@crg.eu>
2016-01-28 19:16:53 +0100

    Uniformly distribute points in circle

--- lib/inits.cuh
+++ lib/inits.cuh
@@ -6,10 +6,9 @@ template<typename Pt> void uniform_circle(int N_CELLS, float mean_distance,
         float r_max = pow(N_CELLS/0.9069, 1./2)*mean_distance/2; // Circle packing
         for (int i = 0; i < N_CELLS; i++) {
             float r = r_max*pow(rand()/(RAND_MAX + 1.), 1./2);
-            float theta = rand()/(RAND_MAX + 1.)*2*M_PI;
-            float phi = acos(2.*rand()/(RAND_MAX + 1.) - 1);
+            float phi = rand()/(RAND_MAX + 1.)*2*M_PI;
             X[i].x = 0;
-            X[i].y = r*cos(theta)*sin(phi);
+            X[i].y = r*sin(phi);
             X[i].z = r*cos(phi);
         }
     }
