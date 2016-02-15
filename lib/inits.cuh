9e0ec953998fb215346951337cb2fc46d3add282
Philipp Germann <philipp.germann@crg.eu>
2016-02-15 17:04:54 +0100

    Fix intendation

--- lib/inits.cuh
+++ lib/inits.cuh
@@ -3,15 +3,15 @@
 // Distribute first N_CELLS in X[] uniformly random in circle
 template<typename Pt> void uniform_circle(int N_CELLS, float mean_distance,
     Pt X[]) {
-        float r_max = pow(N_CELLS/0.9069, 1./2)*mean_distance/2; // Circle packing
-        for (int i = 0; i < N_CELLS; i++) {
-            float r = r_max*pow(rand()/(RAND_MAX + 1.), 1./2);
-            float phi = rand()/(RAND_MAX + 1.)*2*M_PI;
-            X[i].x = 0;
-            X[i].y = r*sin(phi);
-            X[i].z = r*cos(phi);
-        }
+    float r_max = pow(N_CELLS/0.9069, 1./2)*mean_distance/2; // Circle packing
+    for (int i = 0; i < N_CELLS; i++) {
+        float r = r_max*pow(rand()/(RAND_MAX + 1.), 1./2);
+        float phi = rand()/(RAND_MAX + 1.)*2*M_PI;
+        X[i].x = 0;
+        X[i].y = r*sin(phi);
+        X[i].z = r*cos(phi);
     }
+}
 
 // Distribute first N_CELLS in X[] uniformly random in sphere
 template<typename Pt> void uniform_sphere(int N_CELLS, float mean_distance,
