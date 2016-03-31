e5131bf2dd3cdf0e770a580371bc9b6f354303ca
Philipp Germann <philipp.germann@crg.eu>
2016-03-31 11:36:18 +0200

    Test for cells aligning

--- tests/test_epithelium.cu
+++ tests/test_epithelium.cu
@@ -37,14 +37,26 @@ const char* test_line_of_four() {
         X[i].theta = M_PI/2;
     }
     float3 com_i = center_of_mass(4, X);
-    for (int i = 0; i < 250; i++) {
-        X.step(1, potential, 4);
+    for (int i = 0; i < 500; i++) {
+        X.step(0.5, potential, 4);
     }
     for (int i = 1; i < 4; i++) {
         float prod = sinf(X[0].theta)*sinf(X[i].theta)*cosf(X[0].phi - X[i].phi)
             + cosf(X[0].theta)*cosf(X[i].theta);
         mu_assert("ERROR: Polarity not aligned", mu_isclose(prod, 1));
     }
+    float3 r_01 = {X[1].x - X[0].x, X[1].y - X[0].y, X[1].z - X[0].z};
+    float3 r_12 = {X[2].x - X[1].x, X[2].y - X[1].y, X[2].z - X[1].z};
+    float3 r_23 = {X[3].x - X[2].x, X[3].y - X[2].y, X[3].z - X[2].z};
+    printf("%f %f\n", r_01.x, r_12.x);
+    printf("%f %f\n", r_01.y, r_12.y);
+    printf("%f %f\n", r_01.z, r_12.z);
+    mu_assert("ERROR: Cells not on line", mu_isclose(r_01.x, r_12.x));
+    mu_assert("ERROR: Cells not on line", mu_isclose(r_12.x, r_23.x));
+    mu_assert("ERROR: Cells not on line", mu_isclose(r_01.y, r_12.y));
+    mu_assert("ERROR: Cells not on line", mu_isclose(r_12.y, r_23.y));
+    mu_assert("ERROR: Cells not on line", mu_isclose(r_01.z, r_12.z));
+    mu_assert("ERROR: Cells not on line", mu_isclose(r_12.z, r_23.z));
     float3 com_f = center_of_mass(4, X);
     mu_assert("ERROR: Momentum in line", mu_isclose(com_i.x, com_f.x));
     mu_assert("ERROR: Momentum in line", mu_isclose(com_i.y, com_f.y));
