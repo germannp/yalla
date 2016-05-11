71e9767f82bb562e7e36023251fbd8717a895b96
Philipp Germann <philipp.germann@crg.eu>
2016-05-11 17:27:24 +0200

    Overload operators as members

--- lib/dtypes.cuh
+++ lib/dtypes.cuh
@@ -28,14 +28,13 @@ __device__ float4 operator*(const float4& a, const float b) {
 // Polarized cell
 struct pocell {
     float x, y, z, phi, theta;
+
+    friend __device__ pocell operator+(const pocell& a, const pocell& b) {
+        pocell sum = {a.x + b.x, a.y + b.y, a.z + b.z, a.phi + b.phi, a.theta + b.theta};
+        return sum;
+    }
+    friend __device__ pocell operator*(const pocell& a, const float b) {
+        pocell prod = {a.x*b, a.y*b, a.z*b, a.phi*b, a.theta*b};
+        return prod;
+    }
 };
-
-__device__ pocell operator+(const pocell& a, const pocell& b) {
-    pocell sum = {a.x + b.x, a.y + b.y, a.z + b.z, a.phi + b.phi, a.theta + b.theta};
-    return sum;
-}
-
-__device__ pocell operator*(const pocell& a, const float b) {
-    pocell prod = {a.x*b, a.y*b, a.z*b, a.phi*b, a.theta*b};
-    return prod;
-}
