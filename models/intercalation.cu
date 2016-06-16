a6f3fb1316d9d84c9c94087294fde3955b31b8a7
Philipp Germann <philipp.germann@crg.eu>
2016-06-16 11:56:44 +0200

    Use auto where appropriate

--- models/intercalation.cu
+++ models/intercalation.cu
@@ -7,12 +7,12 @@
 #include "../lib/vtk.cuh"
 
 
-const float R_MAX = 1;
-const float R_MIN = 0.5;
-const int N_CELLS = 500;
-const int N_CONNECTIONS = 250;
-const int N_TIME_STEPS = 1000;
-const float DELTA_T = 0.2;
+const auto R_MAX = 1.f;
+const auto R_MIN = 0.5f;
+const auto N_CELLS = 500u;
+const auto N_CONNECTIONS = 250u;
+const auto N_TIME_STEPS = 1000u;
+const auto DELTA_T = 0.2f;
 
 __device__ __managed__ Solution<float3, N_CELLS, LatticeSolver> X;
 __device__ __managed__ int connections[N_CONNECTIONS][2];
@@ -23,11 +23,11 @@ __device__ float3 clipped_cubic(float3 Xi, float3 Xj, int i, int j) {
     float3 dF {0};
     if (i == j) return dF;
 
-    float3 r = Xi - Xj;
-    float dist = sqrtf(r.x*r.x + r.y*r.y + r.z*r.z);
+    auto r = Xi - Xj;
+    auto dist = sqrtf(r.x*r.x + r.y*r.y + r.z*r.z);
     if (dist > R_MAX) return dF;
 
-    float F = 2*(R_MIN - dist)*(R_MAX - dist) + (R_MAX - dist)*(R_MAX - dist);
+    auto F = 2*(R_MIN - dist)*(R_MAX - dist) + (R_MAX - dist)*(R_MAX - dist);
     dF = r*F/dist;
     return dF;
 }
@@ -44,10 +44,10 @@ __global__ void intercalate(const float3* __restrict__ X, float3* dX) {
     auto i = blockIdx.x*blockDim.x + threadIdx.x;
     if (i >= N_CONNECTIONS) return;
 
-    float3 Xi = X[connections[i][0]];
-    float3 Xj = X[connections[i][1]];
-    float3 r = Xi - Xj;
-    float dist = sqrtf(r.x*r.x + r.y*r.y + r.z*r.z);
+    auto Xi = X[connections[i][0]];
+    auto Xj = X[connections[i][1]];
+    auto r = Xi - Xj;
+    auto dist = sqrtf(r.x*r.x + r.y*r.y + r.z*r.z);
 
     atomicAdd(&dX[connections[i][0]].x, -r.x/dist/5);
     atomicAdd(&dX[connections[i][0]].y, -r.y/dist/5);
@@ -66,10 +66,10 @@ __global__ void update_connections() {
     auto i = blockIdx.x*blockDim.x + threadIdx.x;
     if (i >= N_CONNECTIONS) return;
 
-    int j = static_cast<int>(curand_uniform(&rand_states[i])*N_CELLS);
-    int k = static_cast<int>(curand_uniform(&rand_states[i])*N_CELLS);
-    float3 r = X[j] - X[k];
-    float dist = sqrtf(r.x*r.x + r.y*r.y + r.z*r.z);
+    auto j = static_cast<int>(curand_uniform(&rand_states[i])*N_CELLS);
+    auto k = static_cast<int>(curand_uniform(&rand_states[i])*N_CELLS);
+    auto r = X[j] - X[k];
+    auto dist = sqrtf(r.x*r.x + r.y*r.y + r.z*r.z);
     if ((fabs(r.x/dist) < 0.2) and (j != k) and (dist < 2)) {
         connections[i][0] = j;
         connections[i][1] = k;
@@ -84,10 +84,10 @@ int main(int argc, char const *argv[]) {
     cudaDeviceSynchronize();
     int i = 0;
     while (i < N_CONNECTIONS) {
-        int j = static_cast<int>(rand()/(RAND_MAX + 1.)*N_CELLS);
-        int k = static_cast<int>(rand()/(RAND_MAX + 1.)*N_CELLS);
-        float3 r = X[j] - X[k];
-        float dist = sqrtf(r.x*r.x + r.y*r.y + r.z*r.z);
+        auto j = static_cast<int>(rand()/(RAND_MAX + 1.)*N_CELLS);
+        auto k = static_cast<int>(rand()/(RAND_MAX + 1.)*N_CELLS);
+        auto r = X[j] - X[k];
+        auto dist = sqrtf(r.x*r.x + r.y*r.y + r.z*r.z);
         if ((fabs(r.x/dist) < 0.2) and (j != k) and (dist < 2)) {
             connections[i][0] = j;
             connections[i][1] = k;
@@ -97,7 +97,7 @@ int main(int argc, char const *argv[]) {
 
     // Integrate cell positions
     VtkOutput output("intercalation");
-    for (int time_step = 0; time_step <= N_TIME_STEPS; time_step++) {
+    for (auto time_step = 0; time_step <= N_TIME_STEPS; time_step++) {
         output.write_positions(X);
         output.write_connections(connections);
         if (time_step == N_TIME_STEPS) return 0;
