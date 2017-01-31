0ce1430d5cc68247a500ce5b8f7e51bc060cfa5c
Philipp Germann <philipp.germann@crg.eu>
2017-01-31 18:11:35 +0100

    Fix protrusions to cells

--- models/intercalation.cu
+++ models/intercalation.cu
@@ -11,7 +11,7 @@
 const auto r_max = 1.f;
 const auto r_min = 0.5f;
 const auto n_cells = 500u;
-const auto n_protrusions = 250u;
+const auto prots_per_cell = 1;
 const auto n_time_steps = 1000u;
 const auto dt = 0.2f;
 
@@ -35,9 +35,9 @@ __device__ float3 pairwise_interaction(float3 Xi, float3 Xj, int i, int j) {
 __global__ void update_protrusions(const float3* __restrict__ d_X, Link* d_link,
         curandState* d_state) {
     auto i = blockIdx.x*blockDim.x + threadIdx.x;
-    if (i >= n_protrusions) return;
+    if (i >= n_cells*prots_per_cell) return;
 
-    auto j = min(static_cast<int>(curand_uniform(&d_state[i])*n_cells), n_cells - 1);
+    auto j = static_cast<int>((i + 0.5)/prots_per_cell);
     auto k = min(static_cast<int>(curand_uniform(&d_state[i])*n_cells), n_cells - 1);
     if (j == k) return;
 
@@ -54,30 +54,17 @@ int main(int argc, char const *argv[]) {
     // Prepare initial state
     Solution<float3, n_cells, Lattice_solver> bolls;
     uniform_sphere(r_min, bolls);
-    Links<n_protrusions> protrusions;
-    auto intercalation = std::bind(linear_force<n_protrusions>, protrusions,
+    Links<n_cells*prots_per_cell> protrusions;
+    auto intercalation = std::bind(linear_force<n_cells*prots_per_cell>, protrusions,
         std::placeholders::_1, std::placeholders::_2);
-    int i = 0;
-    while (i < n_protrusions) {
-        auto j = static_cast<int>(rand()/(RAND_MAX + 1.)*n_cells);
-        auto k = static_cast<int>(rand()/(RAND_MAX + 1.)*n_cells);
-        auto r = bolls.h_X[j] - bolls.h_X[k];
-        auto dist = sqrtf(r.x*r.x + r.y*r.y + r.z*r.z);
-        if ((fabs(r.x/dist) < 0.2) and (j != k) and (dist < 2)) {
-            protrusions.h_link[i].a = j;
-            protrusions.h_link[i].b = k;
-            i++;
-        }
-    }
-    protrusions.copy_to_device();
 
     // Integrate cell positions
     Vtk_output output("intercalation");
     for (auto time_step = 0; time_step <= n_time_steps; time_step++) {
         bolls.copy_to_host();
         protrusions.copy_to_host();
-        update_protrusions<<<(n_protrusions + 32 - 1)/32, 32>>>(bolls.d_X, protrusions.d_link,
-            protrusions.d_state);
+        update_protrusions<<<(n_cells*prots_per_cell + 32 - 1)/32, 32>>>(bolls.d_X,
+            protrusions.d_link, protrusions.d_state);
         bolls.take_step(dt, intercalation);
         output.write_positions(bolls);
         output.write_links(protrusions);
