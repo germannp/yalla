2411ec47330f66da17353c1314c2fb49cd53b6d7
mmarinriera <miquel.marin15@gmail.com>
2017-05-30 11:30:00 +0200

    Model: Turing mechanism on 2D epithelium

---
+++ examples/turing_2D.cu
@@ -0,0 +1,90 @@
+// Simulate a Turing mechanism within a 2D epithelium
+// Meinhard equations
+
+#include "../include/dtypes.cuh"
+#include "../include/inits.cuh"
+#include "../include/solvers.cuh"
+#include "../include/vtk.cuh"
+#include "../include/polarity.cuh"
+
+
+const auto r_max = 1;
+const auto r_min = 0.6;
+const auto n_cells = 500;
+const auto n_time_steps = 10000;
+const auto lambda = 1.;
+//Turing parameters
+const auto D_v = 4.0;
+const auto f_v = 1.0;
+const auto f_u = 80.0;
+const auto g_u = 40.0;
+const auto m_u = 0.25;
+const auto m_v = 0.5;
+const auto s_u = 0.05;
+const auto D_u = 0.1;
+//
+const auto dt = 0.05*r_min*r_min/D_v;
+
+
+MAKE_PT(Epi_cell, x, y, z, theta, phi, u, v);
+
+
+__device__ Epi_cell epithelium_w_turing(Epi_cell Xi, Epi_cell Xj, int i, int j) {
+    Epi_cell dF {0};
+
+    // Turing reactions
+    if (i == j) {
+        //Meinhard equations
+        dF.u = lambda*((f_u*Xi.u*Xi.u)/(1+f_v*Xi.v)-m_u*Xi.u + s_u);
+        dF.v = lambda*(g_u*Xi.u*Xi.u - m_v*Xi.v);
+
+        return dF;
+    }
+
+    // Diffusion & mechanics
+    auto r = Xi - Xj;
+    auto dist = norm3df(r.x, r.y, r.z);
+    if (dist > r_max) return dF;
+
+    dF.u = -D_u*r.u;
+    dF.v = -D_v*r.v;
+
+    auto F = 2*(r_min - dist)*(r_max - dist) + powf(r_max - dist, 2);
+    dF.x = r.x*F/dist;
+    dF.y = r.y*F/dist;
+    dF.z = r.z*F/dist;
+
+    dF += rigidity_force(Xi, Xj)*3;
+    return dF;
+}
+
+
+int main(int argc, char const *argv[]) {
+    // Prepare initial state
+
+    Solution<Epi_cell, n_cells, Lattice_solver> bolls;
+    for (int i = 0; i < n_cells; i++) {
+        bolls.h_X[i].theta = M_PI/2;
+        bolls.h_X[i].u = rand()/(RAND_MAX + 1.)/5 - 0.1;
+        bolls.h_X[i].v = rand()/(RAND_MAX + 1.)/5 - 0.1;
+    }
+    uniform_circle(0.5, bolls);
+
+
+    // Integrate positions
+    int write_interval=100;
+    Vtk_output output("turing2D");
+    for (auto time_step = 0; time_step <= n_time_steps; time_step++) {
+        bolls.copy_to_host();
+        bolls.take_step<epithelium_w_turing>(dt);
+        if(time_step%write_interval==0)
+        {
+          output.write_positions(bolls);
+          output.write_polarity(bolls);
+          output.write_field(bolls, "u", &Epi_cell::u);
+          output.write_field(bolls, "v", &Epi_cell::v);
+        }
+    }
+
+    return 0;
+}
