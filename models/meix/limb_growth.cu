cfa84d9906f50d95c868dc20b81fbaa9e92a2ec7
mmarinriera <miquel.marin15@gmail.com>
2017-07-17 18:02:20 +0200

    Included intercalation.

--- models/meix/limb_growth.cu
+++ models/meix/limb_growth.cu
@@ -5,6 +5,7 @@
 // argv[2]=output file tag
 // argv[3]=proliferation rate
 // argv[4]=time steps
+// argv[4]=proliferation rate distr. (0=uniform, 1=PD gradient)
 
 #include <curand_kernel.h>
 #include "../../include/dtypes.cuh"
@@ -24,6 +25,9 @@ const auto r_max=1.0;
 const auto r_min=0.8;
 const auto dt = 0.05*r_min*r_min;
 const auto n_max = 150000;
+const auto prots_per_cell=1;
+const auto protrusion_strength=4.0f;
+const auto r_protrusion=2.0f;
 
 enum Cell_types {mesenchyme, epithelium};
 
@@ -35,12 +39,17 @@ __device__ float* d_prolif_rate;
 Property<n_max, int> n_mes_nbs("n_mes_nbs"); //defining these here so function
 Property<n_max, int> n_epi_nbs("n_epi_nbs"); //"neighbour_init" can see them
 
-MAKE_PT(Cell, theta, phi);
+MAKE_PT(Cell, w, f, theta, phi);
 
 __device__ Cell wall_force(Cell Xi, Cell r, float dist, int i, int j) {
     Cell dF {0};
 
-    if(i==j) return dF;
+    if (i == j) {
+        // D_ASSERT(Xi.w >= 0);
+        dF.w = - 0.01*(d_type[i] == mesenchyme)*Xi.w;
+        dF.f = - 0.01*(d_type[i] == mesenchyme)*Xi.f;
+        return dF;
+    }
 
     if (dist > r_max) return dF;
 
@@ -55,6 +64,9 @@ __device__ Cell wall_force(Cell Xi, Cell r, float dist, int i, int j) {
     dF.x = r.x*F/dist;
     dF.y = r.y*F/dist;
     dF.z = r.z*F/dist;
+
+    dF.w = - r.w*(d_type[i] == mesenchyme)*0.5f;
+    dF.f = - r.f*(d_type[i] == mesenchyme)*0.5f;
 
     if(d_type[i]==epithelium && d_type[j]==epithelium) dF += rigidity_force(Xi, r, dist)*0.5f;
 
@@ -71,6 +83,43 @@ __device__ Cell wall_force(Cell Xi, Cell r, float dist, int i, int j) {
 __device__ float wall_friction(Cell Xi, Cell r, float dist, int i, int j) {
     if(Xi.x<0) return 0;
     return 1;
+}
+
+__global__ void update_protrusions(const int n_cells, const Grid<n_max>* __restrict__ d_grid,
+        const Cell* __restrict d_X, curandState* d_state, Link* d_link) {
+    auto i = blockIdx.x*blockDim.x + threadIdx.x;
+    if (i >= n_cells*prots_per_cell) return;
+
+    auto j = static_cast<int>((i + 0.5)/prots_per_cell);
+    auto rand_nb_cube = d_grid->d_cube_id[j]
+        + d_moore_nhood[min(static_cast<int>(curand_uniform(&d_state[i])*27), 26)];
+    auto cells_in_cube = d_grid->d_cube_end[rand_nb_cube] - d_grid->d_cube_start[rand_nb_cube];
+    if (cells_in_cube < 1) return;
+
+    auto a = d_grid->d_point_id[j];
+    auto b = d_grid->d_point_id[d_grid->d_cube_start[rand_nb_cube]
+        + min(static_cast<int>(curand_uniform(&d_state[i])*cells_in_cube), cells_in_cube - 1)];
+    D_ASSERT(a >= 0); D_ASSERT(a < n_cells);
+    D_ASSERT(b >= 0); D_ASSERT(b < n_cells);
+    if (a == b) return;
+
+    if ((d_type[a] != mesenchyme) or (d_type[b] != mesenchyme)) return;
+
+    auto new_r = d_X[a] - d_X[b];
+    auto new_dist = norm3df(new_r.x, new_r.y, new_r.z);
+    if (new_dist > r_protrusion) return;
+
+    auto link = &d_link[a*prots_per_cell + i%prots_per_cell];
+    auto not_initialized = link->a == link->b;
+    auto old_r = d_X[link->a] - d_X[link->b];
+    auto old_dist = norm3df(old_r.x, old_r.y, old_r.z);
+    auto noise = curand_uniform(&d_state[i]);
+    auto more_along_w = fabs(new_r.w/new_dist) > fabs(old_r.w/old_dist)*(1.f - noise);
+    auto high_f = (d_X[a].f + d_X[b].f) > 0.05;
+    if (not_initialized or more_along_w or high_f) {
+        link->a = a;
+        link->b = b;
+    }
 }
 
 __global__ void proliferate(float max_rate, float mean_distance, Cell* d_X, int* d_n_cells,
@@ -101,6 +150,15 @@ __global__ void proliferate(float max_rate, float mean_distance, Cell* d_X, int*
     d_X[n].x = d_X[i].x + mean_distance/4*sinf(theta)*cosf(phi);
     d_X[n].y = d_X[i].y + mean_distance/4*sinf(theta)*sinf(phi);
     d_X[n].z = d_X[i].z + mean_distance/4*cosf(theta);
+    if (d_type[i] == mesenchyme) {
+        d_X[n].w = d_X[i].w/2;
+        d_X[i].w = d_X[i].w/2;
+        d_X[n].f = d_X[i].f/2;
+        d_X[i].f = d_X[i].f/2;
+    } else {
+        d_X[n].w = d_X[i].w;
+        d_X[n].f = d_X[i].f;
+    }
     d_X[n].theta = d_X[i].theta;
     d_X[n].phi = d_X[i].phi;
     d_type[n] = d_type[i];
@@ -116,6 +174,14 @@ void neighbour_init(const Cell* __restrict__ d_X, Cell* d_dX) {
     thrust::fill(thrust::device, n_mes_nbs.d_prop, n_mes_nbs.d_prop + n_max, 0);
 }
 
+template<int n_links, typename Pt = float3, Link_force<Pt> force = linear_force<Pt>>
+void link_forces_w_n_init(Links<n_links>& links, const Pt* __restrict__ d_X, Pt* d_dX) {
+    thrust::fill(thrust::device, n_epi_nbs.d_prop, n_epi_nbs.d_prop + n_max, 0);
+    thrust::fill(thrust::device, n_mes_nbs.d_prop, n_mes_nbs.d_prop + n_max, 0);
+    link<Pt, force><<<(links.get_d_n() + 32 - 1)/32, 32>>>(
+        d_X, d_dX, links.d_link, links.get_d_n(), links.strength);
+}
+
 //*****************************************************************************
 
 int main(int argc, char const *argv[]) {
@@ -124,6 +190,7 @@ int main(int argc, char const *argv[]) {
     std::string output_tag=argv[2];
     float max_proliferation_rate=std::stof(argv[3]);
     int n_time_steps=std::stoi(argv[4]);
+    int prolif_dist=std::stoi(argv[5]);
 
     //Load the initial conditions
     Vtk_input input(file_name);
@@ -140,7 +207,11 @@ int main(int argc, char const *argv[]) {
     input.read_property(intype); //we read it as an int, then we translate to enum "Cell_types"
     for(int i=0 ; i<n0 ; i++){
         if(intype.h_prop[i]==0) type.h_prop[i]=mesenchyme;
-        else type.h_prop[i]=epithelium;
+        else{
+            type.h_prop[i]=epithelium;
+            limb.h_X[i].w=1.0f;
+        }
+
     }
 
     limb.copy_to_device();
@@ -151,23 +222,31 @@ int main(int argc, char const *argv[]) {
     cudaMemcpyToSymbol(d_mes_nbs, &n_mes_nbs.d_prop, sizeof(d_mes_nbs));
     cudaMemcpyToSymbol(d_epi_nbs, &n_epi_nbs.d_prop, sizeof(d_epi_nbs));
 
+    Links<static_cast<int>(n_max*prots_per_cell)> protrusions(protrusion_strength,
+        n0*prots_per_cell);
+    auto intercalation = std::bind(
+        link_forces_w_n_init<static_cast<int>(n_max*prots_per_cell), Cell>,
+        protrusions, std::placeholders::_1, std::placeholders::_2);
+
+    Grid<n_max> grid;
+
     //determine cell-specific proliferation rates
     Property<n_max, float> prolif_rate("prolif_rate");
     cudaMemcpyToSymbol(d_prolif_rate, &prolif_rate.d_prop, sizeof(d_prolif_rate));
-    float xmax=-10000.0f;
-    // float ymax=-10000.0f, ymin=10000.f;
-    for(int i=0 ; i<n0 ; i++) {
-        if(limb.h_X[i].x>xmax) xmax=limb.h_X[i].x;
-        // if(limb.h_X[i].y>ymax) ymax=limb.h_X[i].y;
-        // if(limb.h_X[i].y<ymin) ymin=limb.h_X[i].y;
+    if(prolif_dist==0) {
+        for (int i=0; i<n0 ; i++) {
+            prolif_rate.h_prop[i]=max_proliferation_rate;
+        }
+    }else{
+        float xmax=-10000.0f;
+        for(int i=0 ; i<n0 ; i++) {
+            if(limb.h_X[i].x>xmax) xmax=limb.h_X[i].x;
+        }
+        for (int i=0; i<n0 ; i++) {
+            if(limb.h_X[i].x<0) prolif_rate.h_prop[i]=0;
+            else prolif_rate.h_prop[i]=pow((limb.h_X[i].x/xmax),3)*max_proliferation_rate;
+        }
     }
-    for (int i=0; i<n0 ; i++) {
-        if(limb.h_X[i].x<0) prolif_rate.h_prop[i]=0;
-        else prolif_rate.h_prop[i]=pow((limb.h_X[i].x/xmax),3)*max_proliferation_rate;
-    }
-    // for (int i=0; i<n0 ; i++) {
-    //     prolif_rate.h_prop[i]=pow((limb.h_X[i].y-ymin)/(ymax-ymin),2)*max_proliferation_rate;
-    // }
     prolif_rate.copy_to_device();
 
     // State for proliferations
@@ -183,6 +262,7 @@ int main(int argc, char const *argv[]) {
     for (auto time_step = 0; time_step <= n_time_steps; time_step++) {
         if(time_step%skip_step==0 || time_step==n_time_steps){
             limb.copy_to_host();
+            protrusions.copy_to_host();
             type.copy_to_host();
             n_epi_nbs.copy_to_host();
             n_mes_nbs.copy_to_host();
@@ -190,13 +270,19 @@ int main(int argc, char const *argv[]) {
         }
 
         proliferate<<<(limb.get_d_n() + 128 - 1)/128, 128>>>(max_proliferation_rate, r_min, limb.d_X, limb.d_n, d_state);
+        protrusions.set_d_n(limb.get_d_n()*prots_per_cell);
+        grid.build(limb, r_protrusion);
+        update_protrusions<<<(protrusions.get_d_n() + 32 - 1)/32, 32>>>(limb.get_d_n(),
+            grid.d_grid, limb.d_X, protrusions.d_state, protrusions.d_link);
 
-        limb.take_step<wall_force, wall_friction>(dt, neighbour_init);
+        limb.take_step<wall_force, wall_friction>(dt, intercalation);
 
         //write the output
         if(time_step%skip_step==0 || time_step==n_time_steps) {
             limb_output.write_positions(limb);
+            limb_output.write_links(protrusions);
             limb_output.write_polarity(limb);
+            limb_output.write_field(limb, "Wint");
             limb_output.write_property(type);
             limb_output.write_property(n_epi_nbs);
             limb_output.write_property(n_mes_nbs);
