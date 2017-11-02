41ffafd3bf7df8170c6607688169fb731866cc25
Philipp Germann <philipp.germann@crg.eu>
2017-11-02 12:20:03 +0100

    Simplify PCP example

--- examples/pcp.cu
+++ examples/pcp.cu
@@ -9,17 +9,14 @@
 const auto r_max = 1;
 const auto r_min = 0.6;
 const auto D = 0.5f;
-const auto n_cells = 500;
+const auto n_cells = 200;
 const auto n_time_steps = 300;
 const auto dt = 0.025;
 
-MAKE_PT(Po_cell4, w, theta, phi);
 
-
-__device__ Po_cell4 biased_pcp(
-    Po_cell4 Xi, Po_cell4 r, float dist, int i, int j)
+__device__ Po_cell pcp(Po_cell Xi, Po_cell r, float dist, int i, int j)
 {
-    Po_cell4 dF{0};
+    Po_cell dF{0};
     if (i == j) return dF;
 
     if (dist > r_max) return dF;
@@ -32,11 +29,6 @@ __device__ Po_cell4 biased_pcp(
 
     // U_PCP = - Σ(n_i . n_j)^2/2
     dF += pcp_force(Xi, Xi - r);
-    if (r.w > 0) return dF;
-
-    // U_WNT = - ΣXj.w*(n_i . r_ij/r)^2/2 to bias along w
-    Polarity rhat{acosf(-r.z / dist), atan2(-r.y, -r.x)};
-    dF += (Xi.w - r.w) * pcp_force(Xi, rhat);
     return dF;
 }
 
@@ -44,7 +36,7 @@ __device__ Po_cell4 biased_pcp(
 int main(int argc, const char* argv[])
 {
     // Prepare initial state
-    Solution<Po_cell4, n_cells, Grid_solver> bolls;
+    Solution<Po_cell, n_cells, Grid_solver> bolls;
     for (auto i = 0; i < n_cells; i++) {
         bolls.h_X[i].w = (i == 0) * 10;
         bolls.h_X[i].theta = acos(2. * rand() / (RAND_MAX + 1.) - 1.);
@@ -56,9 +48,8 @@ int main(int argc, const char* argv[])
     Vtk_output output("pcp");
     for (auto time_step = 0; time_step <= n_time_steps; time_step++) {
         bolls.copy_to_host();
-        bolls.take_step<biased_pcp>(dt);
+        bolls.take_step<pcp>(dt);
         output.write_positions(bolls);
-        output.write_field(bolls);
         output.write_polarity(bolls);
     }
 
