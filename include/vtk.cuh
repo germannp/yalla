92f3f281a82bfd9ccca98db0fa2150d88e8d413e
Miquel Marin-Riera <miquel.marin15@gmail.com>
2019-01-10 17:05:04 +0100

    Now the vtkoutput class can take boolean masks as argument. If you pass a pointer to an array of bool to the vtkoutput constructor, it will use it to sort which points of the Solution class are actually written to the output file and which are not. The boolean mask needs to be built outside the class according to the criterion of choice. If no boolean mask is passed, the vtkoutput class writes down all the cells.

--- include/vtk.cuh
+++ include/vtk.cuh
@@ -28,6 +28,8 @@ struct Property;
 
 class Vtk_output {
     int n_points;
+    int n_points_mask;
+    bool* mask = NULL;
     int time_step{0};
     std::string base_name;
     std::string current_path;
@@ -41,7 +43,7 @@ public:
     ~Vtk_output(void);
     // Write x, y, and z component of Pt; has to be written first
     template<typename Pt, template<typename> class Solver>
-    void write_positions(Solution<Pt, Solver>& points);
+    void write_positions(Solution<Pt, Solver>& points, bool* input_mask = NULL);
     // Write links, see links.cuh; if written has to be second
     void write_links(Links& links);
     // Write further components of Pt
@@ -83,9 +85,18 @@ Vtk_output::~Vtk_output()
 }
 
 template<typename Pt, template<typename> class Solver>
-void Vtk_output::write_positions(Solution<Pt, Solver>& points)
+void Vtk_output::write_positions(Solution<Pt, Solver>& points, bool* input_mask)
 {
+    // n_points is the real size of the Solver array, used in the loops,
+    // n_points mask is the size of the subset filtered by the mask, which is
+    // written down on the file
     n_points = *points.h_n;
+    mask = input_mask;
+    if(mask != NULL){
+        n_points_mask = 0;
+        for (auto i = 0; i < n_points; i++)
+            n_points_mask += 1*mask[i];
+    }
 
     current_path =
         "output/" + base_name + "_" + std::to_string(time_step) + ".vtk";
@@ -97,13 +108,30 @@ void Vtk_output::write_positions(Solution<Pt, Solver>& points)
     file << "ASCII\n";
     file << "DATASET POLYDATA\n";
 
-    file << "\nPOINTS " << n_points << " float\n";
-    for (auto i = 0; i < n_points; i++)
-        file << points.h_X[i].x << " " << points.h_X[i].y << " "
-             << points.h_X[i].z << "\n";
+    if(mask==NULL){
+        file << "\nPOINTS " << n_points << " float\n";
+        for (auto i = 0; i < n_points; i++)
+            file << points.h_X[i].x << " " << points.h_X[i].y << " "
+            << points.h_X[i].z << "\n";
 
-    file << "\nVERTICES " << n_points << " " << 2 * n_points << "\n";
-    for (auto i = 0; i < n_points; i++) file << "1 " << i << "\n";
+        file << "\nVERTICES " << n_points << " " << 2 * n_points << "\n";
+        for (auto i = 0; i < n_points; i++) file << "1 " << i << "\n";
+    }else{
+        file << "\nPOINTS " << n_points_mask << " float\n";
+        for (auto i = 0; i < n_points; i++){
+            if(mask[i])
+                file << points.h_X[i].x << " " << points.h_X[i].y << " "
+                << points.h_X[i].z << "\n";
+        }
+        auto j = 0;
+        file << "\nVERTICES " << n_points_mask << " " << 2 * n_points_mask << "\n";
+        for (auto i = 0; i < n_points; i++){
+            if(mask[i]){
+                file << "1 " << j << "\n";
+                j++;
+            }
+        }
+    }
 
     point_data_started = false;
     time_step += 1;
@@ -133,12 +161,23 @@ void Vtk_output::write_field(
     assert(file.is_open());
 
     if (!point_data_started) {
-        file << "\nPOINT_DATA " << n_points << "\n";
+        if(mask==NULL)
+            file << "\nPOINT_DATA " << n_points << "\n";
+        else
+            file << "\nPOINT_DATA " << n_points_mask << "\n";
+
         point_data_started = true;
     }
     file << "SCALARS " << data_name << " float\n";
     file << "LOOKUP_TABLE default\n";
-    for (auto i = 0; i < n_points; i++) file << points.h_X[i].*field << "\n";
+    if(mask==NULL)
+        for (auto i = 0; i < n_points; i++) file << points.h_X[i].*field << "\n";
+    else{
+        for (auto i = 0; i < n_points; i++){
+            if(mask[i])
+                file << points.h_X[i].*field << "\n";
+        }
+    }
 }
 
 template<typename Pt, template<typename> class Solver>
@@ -148,14 +187,28 @@ void Vtk_output::write_polarity(Solution<Pt, Solver>& points)
     assert(file.is_open());
 
     if (!point_data_started) {
-        file << "\nPOINT_DATA " << n_points << "\n";
+        if(mask==NULL)
+            file << "\nPOINT_DATA " << n_points << "\n";
+        else
+            file << "\nPOINT_DATA " << n_points_mask << "\n";
+
         point_data_started = true;
     }
     file << "NORMALS polarity float\n";
-    for (auto i = 0; i < n_points; i++) {
-        auto n = pol_to_float3(points.h_X[i]);
-        if ((points.h_X[i].theta == 0) and (points.h_X[i].phi == 0)) n.z = 0;
-        file << n.x << " " << n.y << " " << n.z << "\n";
+    if(mask==NULL){
+        for (auto i = 0; i < n_points; i++) {
+            auto n = pol_to_float3(points.h_X[i]);
+            if ((points.h_X[i].theta == 0) and (points.h_X[i].phi == 0)) n.z = 0;
+            file << n.x << " " << n.y << " " << n.z << "\n";
+        }
+    }else{
+        for (auto i = 0; i < n_points; i++) {
+            if(mask[i]){
+                auto n = pol_to_float3(points.h_X[i]);
+                if ((points.h_X[i].theta == 0) and (points.h_X[i].phi == 0)) n.z = 0;
+                file << n.x << " " << n.y << " " << n.z << "\n";
+            }
+        }
     }
 }
 
@@ -166,7 +219,11 @@ void Vtk_output::write_property(Property<Prop>& property)
     assert(file.is_open());
 
     if (!point_data_started) {
-        file << "\nPOINT_DATA " << n_points << "\n";
+        if(mask==NULL)
+            file << "\nPOINT_DATA " << n_points << "\n";
+        else
+            file << "\nPOINT_DATA " << n_points_mask << "\n";
+
         point_data_started = true;
     }
 
@@ -179,7 +236,14 @@ void Vtk_output::write_property(Property<Prop>& property)
     assert(n_points <= property.n_max);
     file << "SCALARS " << property.name << " " << ptype << "\n";
     file << "LOOKUP_TABLE default\n";
-    for (auto i = 0; i < n_points; i++) file << property.h_prop[i] << "\n";
+    if(mask == NULL)
+        for (auto i = 0; i < n_points; i++) file << property.h_prop[i] << "\n";
+    else{
+        for (auto i = 0; i < n_points; i++){
+            if(mask[i])
+                file << property.h_prop[i] << "\n";
+        }
+    }
 }
 
 
