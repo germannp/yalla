63a63ca278a39ec9ab6e2fa57cc9c0adb6de8a07
Philipp Germann <philipp.germann@crg.eu>
2019-01-24 16:06:04 +0100

    Merge tests for links.cuh

--- tests/test_links.cu
+++ tests/test_links.cu
@@ -4,14 +4,15 @@
 #include "minunit.cuh"
 
 
-__device__ float3 no_pw_int(float3 Xi, float3 r, float dist, int i, int j)
+template<Pt>
+__device__ float3 no_pw_int(Pt Xi, Pt r, float dist, int i, int j)
 {
-    float3 dF{0};
+    Pt dF{0};
     return dF;
 }
 
 
-const char* square_of_four()
+const char* test_square_of_four()
 {
     Solution<float3, Tile_solver> points{4};
     Links links{4};
@@ -49,9 +50,53 @@ const char* square_of_four()
 }
 
 
+template<typename Pt>
+__device__ void custom_force(const Pt* __restrict__ d_X, const int a,
+    const int b, const float strength, Pt* d_dX)
+{
+    atomicAdd(&d_dX[a].w, -1);
+    atomicAdd(&d_dX[b].w, 1);
+}
+
+const char* test_custom_force()
+{
+    Solution<float4, Tile_solver> points{2};
+    Links links{1};
+
+    auto forces = [&links](const float4* __restrict__ d_X, float4* d_dX) {
+        return link_forces(links, d_X, d_dX);
+    };
+
+    auto custom_forces = [&links](const float4* __restrict__ d_X, float4* d_dX) {
+        return link_forces<float4, custom_force>(links, d_X, d_dX);
+    };
+
+    // clang-format off
+    points.h_X[0].x = 1;  points.h_X[0].y = 1;  points.h_X[0].z = 0; points.h_X[0].w = 1;
+    points.h_X[1].x = 1;  points.h_X[1].y = -1; points.h_X[1].z = 0; points.h_X[1].w = -1;
+    links.h_link[0].a = 0; links.h_link[0].b = 1;
+    // clang-format on
+    points.copy_to_device();
+    links.copy_to_device();
+
+    auto dt = 0.1;
+    points.take_step<no_pw_int>(dt, forces);
+    points.take_step<no_pw_int>(dt, custom_forces);
+
+    points.copy_to_host();
+    MU_ASSERT("Wrong x", isclose(points.h_X[0].x - points.h_X[1].x, 0));
+    MU_ASSERT("Wrong y", isclose(points.h_X[0].y - points.h_X[1].y, 2 - 2 * dt * links.strength));
+    MU_ASSERT("Wrong z", isclose(points.h_X[0].z - points.h_X[1].z, 0));
+    MU_ASSERT("Wrong w", isclose(points.h_X[0].w - points.h_X[1].w, 2 - 2 * dt));
+
+    return NULL;
+}
+
+
 const char* all_tests()
 {
-    MU_RUN_TEST(square_of_four);
+    MU_RUN_TEST(test_square_of_four);
+    MU_RUN_TEST(test_custom_force);
     return NULL;
 }
 
